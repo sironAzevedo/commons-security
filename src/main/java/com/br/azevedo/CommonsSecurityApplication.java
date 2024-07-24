d21402b6774fb9bc4f2e49db21e33b8e199c77b4
sironAzevedo <sirondba@gmail.com>
2024-07-24 07:46:19 -0300

    primiro commit

---
+++ src/main/java/com/br/azevedo/CommonsSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.br.azevedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/azevedo/CommonsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.azevedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
