91df081e71eaa9386cf6dc650f876becdecf5e61
sironAzevedo <sirondba@gmail.com>
2025-08-30 08:34:30 -0300

    Update VaultSecretManager.java

--- src/main/java/com/br/azevedo/security/secretManager/VaultSecretManager.java
+++ src/main/java/com/br/azevedo/security/secretManager/VaultSecretManager.java
@@ -6,9 +6,11 @@ import com.br.azevedo.security.config.vault.VaultSecretsConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.vault.config.VaultHealthIndicator;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.support.Versioned;
 
@@ -16,7 +18,7 @@ import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
-@Component
+@Configuration
 public class VaultSecretManager {
 
     private final VaultTemplate vaultTemplate;
@@ -24,6 +26,16 @@ public class VaultSecretManager {
     public VaultSecretManager(ApplicationContext applicationContext, Environment environment, VaultParameter vaultParameter) {
         VaultSecretsConfig vaultSecretsConfig = new VaultSecretsConfig(applicationContext, environment);
         this.vaultTemplate = vaultSecretsConfig.vaultTemplate(vaultParameter.getVaultUri(), vaultParameter.getRoleId(), vaultParameter.getSecretId());
+    }
+
+    @Bean
+    public VaultTemplate vaultTemplate() {
+        return this.vaultTemplate;
+    }
+
+    @Bean
+    public VaultHealthIndicator vaultHealthIndicator(VaultTemplate vaultTemplate) {
+        return new VaultHealthIndicator(vaultTemplate);
     }
 
     public void createOrUpdateSecret(String path, Map<String, Object> data) {
